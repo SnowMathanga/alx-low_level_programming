b1916deab13d24f98b6e090485240fd1030a6a6f
SnowMathanga <mathangagoitseone@gmail.com>
2022-10-15 06:14:10 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * fuction that prints out with a new line
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
