77418529de1ae489e23539ca4b9c66c2e3a26eaf
SnowMathanga <mathangagoitseone@gmail.com>
2022-10-19 05:50:25 -0700

    stuff

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet - cHeck Description
+ *Decription: Itprints the alphabet in lower case follwed by a new line
+ *Return: Nothing.
+ */
+
+void print_alphabet(void)
+{
+	 char i;
+
+	 fort (i = 'a'; i <= 'z'; i++)
+	 {
+	 	_putchar(i);
+	 }
+	 _putchar('\n');
+}
