491886439336e242445f72932be037e927c6aa1d
SnowMathanga <mathangagoitseone@gmail.com>
2022-10-21 03:01:28 -0700

    More nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - a function that draws a stratight line in the terminal
+ * followed by a new line
+ * @n: An integer
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int 1 = 0;
+
+	if (n > 0)
+	{
+		for (; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
